b2380ce9d7398354242f8c6eb5bb2f8a19668022
Andreas Hennig <mail@andreas-hennig.me>
2015-05-19 18:39:38 +0200

    works! :)

--- PlanetZoom/src/engine/CoreEngine.java
+++ PlanetZoom/src/engine/CoreEngine.java
@@ -20,7 +20,7 @@ public class CoreEngine
     public boolean running;
     public long windowHandle;
   
-    boolean fullscreen = true;
+    boolean fullscreen = false;
     
     int windowWidth = 800; 
     int windowHeight = 600;
