3bb3de22173098c5e16a269d3b48dcde374b3692
Andreas Hennig <mail@andreas-hennig.me>
2015-06-30 10:47:45 +0200

    Ein funktionierender Stand der Noise-Ueberlagerung vorhanden.

--- PlanetZoom/src/engine/Planet.java
+++ PlanetZoom/src/engine/Planet.java
@@ -5,13 +5,11 @@ import org.lwjgl.util.vector.Vector4f;
 
 import engine.utils.GameUtils;
 import geometry.Sphere;
-import geometry.Vertex;
 import geometry.Vertex3D;
 
 public class Planet {
 	private Sphere sphere;
 	private Vector3f position;
-	private int lastNoisedSubdivion;
 
 	public float getRadius() {
 		return sphere.getRadius();
@@ -28,28 +26,28 @@ public class Planet {
 	public Planet(float radius, Vector3f position) {
 		this.position = position;
 		this.sphere = new Sphere();
-
 	}
 
 	public void update(ICamera camera, int subdivisions)
 	{
-		sphere.update(subdivisions, camera.getLookAt()); // where to apply cam?
-			
+		sphere.update(subdivisions, camera.getLookAt());
+		// where to apply cam?
+		
+		// TODO apply noise to sphere
 		int octaves = 4;
 		float lambda = 0.5f;
 		float amplitude = 2.07f;
-			
-		// TODO apply noise to sphere
+
 		for(Vertex3D v : sphere.getVertices()) {
-			double noise = engine.utils.CustomNoise.perlinNoise(v.getPosition().getX(), 
-																v.getPosition().getY(), 
-																v.getPosition().getZ(), 
+			double noise = engine.utils.CustomNoise.perlinNoise(v.getPosition().getX(),
+																v.getPosition().getY(),
+																v.getPosition().getZ(),
 																octaves, lambda, amplitude);
-				
+
 			double color = (float) ((noise + 1) / 2.0);
 			v.setColorRGBA(new Vector4f((float) color, (float) color, (float) color, 1f));
 		}
-		
+
 		sphere.createVAO();
 	}
 
