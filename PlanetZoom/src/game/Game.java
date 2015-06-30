b8f300bb9ebfd58d3bf0781d877fdbf2bd48ff04
Peter <gamblegearbox@googlemail.com>
2015-06-30 19:48:46 +0200

    sun is rendered with textures

--- PlanetZoom/src/game/Game.java
+++ PlanetZoom/src/game/Game.java
@@ -1,8 +1,11 @@
 package game;
 
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
 import static org.lwjgl.opengl.GL11.GL_VENDOR;
 import static org.lwjgl.opengl.GL11.GL_VERSION;
+import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glGetString;
 import static org.lwjgl.opengl.GL20.GL_SHADING_LANGUAGE_VERSION;
 import static org.lwjgl.opengl.GL20.glUseProgram;
@@ -15,11 +18,11 @@ import org.lwjgl.util.vector.Vector3f;
 import engine.CoreEngine;
 import engine.FreeCamera;
 import engine.HeadsUpDisplay;
-import engine.ICamera;
 import engine.IGame;
 import engine.Info;
 import engine.Planet;
 import engine.Renderer;
+import engine.Sun;
 import engine.utils.GameUtils;
 import engine.utils.MatrixUtils;
 import engine.utils.Texture;
@@ -32,16 +35,24 @@ public class Game implements IGame {
 
 	// GAMEOBJECTS
 	private Planet planet;
+	private Sun sun;
 	private HeadsUpDisplay hud;
 
 	// TEXTURES
 	private Texture planetTexture;
+	private Texture sunTexture;
 
 	// SHADERS
 	private ShaderProgram toonShader;
 	private ShaderProgram hudShader;
-
-	public static void main(String[] args) {
+	private ShaderProgram sunShader;
+	
+	// MATRICES
+	private Matrix4f modelViewMatrix;
+	private Matrix4f normalMatrix;
+	
+	public static void main(String[] args) 
+	{
 		game = new CoreEngine(new Game());
 		game.start();
 	}
@@ -51,76 +62,75 @@ public class Game implements IGame {
 		printVersionInfo();
 
 		Info.camera = new FreeCamera(0.0f, 0.0f, 5.0f);
-		
 		Info.projectionMatrix = engine.utils.MatrixUtils.perspectiveProjectionMatrix(fovParam, game.getWindowWidth(), game.getWindowHeight());
-		
+		modelViewMatrix = new Matrix4f();
+		normalMatrix = new Matrix4f();
 		renderer = new Renderer();
 		
+	
 		initTextures();
 		initShaders();
 		initGameObjects();
-
-		planet = new Planet(3f, new Vector3f(0f, 0f, 0f));
+		
 		Info.planet = planet;
 	}
 
-	/**
-	 * loads textures
-	 */
 	private void initTextures() {
-		planetTexture = new Texture("src/res/textures/crypt_wall.png");
+		planetTexture = new Texture("src/res/textures/uv_test.png");
+		sunTexture = new Texture("src/res/textures/sun.png");
 	}
 
-	/**
-	 * loads shaderPrograms
-	 */
 	private void initShaders() {
 		hudShader = new ShaderProgram("HUDShader");
 		toonShader = new ShaderProgram("toonShader");
+		sunShader = new ShaderProgram("sunShader");
 	}
 
 	private void initGameObjects() {
 		planet = new Planet(3f, new Vector3f(0f, 0f, 0f));
+		sun = new Sun(new Vector3f(10.0f, 0.0f, 0.0f));
 		hud = new HeadsUpDisplay(0, 0, "arial_nm.png", Info.camera.getPosition(), new Vector3f(0.0f, 0.0f, 0.0f), 0f, 0, 0, 0);
 	}
 
 	@Override
 	public void update(int deltaTime) {
-		// 3D Rendering.
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //DO NOT MOVE THIS LINE! ....THERE IS A REASON THAT IT IS NOT IN RENDERER;
+		
 		ICameraControl cameraControl = Info.camera.getCameraControl();
 		Info.camera = cameraControl.handleInput(deltaTime);
 
 		float planetCamDistance = GameUtils.getDistanceBetween(planet.getPosition(), Info.camera.getPosition()) - planet.getRadius();
 
-		Matrix4f modelViewMatrix = new Matrix4f();
 		Matrix4f.mul(planet.getMesh().getModelMatrix(), Info.camera.getViewMatrix(), modelViewMatrix);
-		Matrix4f normalMatrix = new Matrix4f();
 		Matrix4f.invert(modelViewMatrix, normalMatrix);
 
 		planet.update(Info.camera, planetCamDistance, false);
 
 		glUseProgram(toonShader.getId());
-
-		toonShader.loadUniformMat4f(Info.projectionMatrix, "projectionMatrix", false);
-		toonShader.loadUniformMat4f(modelViewMatrix, "modelViewMatrix", false);
-		toonShader.loadUniformMat4f(normalMatrix, "normalMatrix", true);
-		toonShader.loadUniformVec3f(Info.camera.getPosition(), "cameraPosition");
-		toonShader.loadUniform1f(planet.getRadius(), "radius");
-		renderer.renderGameObject(planet.getMesh(), planetTexture, GL_TRIANGLES);
-
+			toonShader.loadUniformMat4f(Info.projectionMatrix, "projectionMatrix", false);
+			toonShader.loadUniformMat4f(modelViewMatrix, "modelViewMatrix", false);
+			toonShader.loadUniformMat4f(normalMatrix, "normalMatrix", true);
+			toonShader.loadUniformVec3f(Info.camera.getPosition(), "cameraPosition");
+			toonShader.loadUniform1f(planet.getRadius(), "radius");
+			renderer.renderGameObject(planet.getMesh(), planetTexture, GL_TRIANGLES);
 		glUseProgram(0);
 
-		// 2D Rendering.
+		Matrix4f.mul(sun.getModelMatrix(), Info.camera.getViewMatrix(), modelViewMatrix);
+		
+		glUseProgram(sunShader.getId());
+			sunShader.loadUniformMat4f(Info.projectionMatrix, "projectionMatrix", false);
+			sunShader.loadUniformMat4f(modelViewMatrix, "modelViewMatrix", false);
+			renderer.renderGameObject(sun, sunTexture, GL_TRIANGLES);
+		glUseProgram(0);
+
 		hud.update(Info.camera.getPosition(), Info.camera.getLookAt(), planetCamDistance, planet.getActualTriangleCount(), planet.getTotalTriangleCount(), game.timer.getFPS());
 
 		glUseProgram(hudShader.getId());
-
-		Matrix4f orthographicProjectionMatrix = MatrixUtils.orthographicProjectionMatrix(0, -game.getWindowWidth(), -game.getWindowHeight(), 0.0f, -1.0f, 1.0f);
-
-		hudShader.loadUniformMat4f(orthographicProjectionMatrix, "projectionMatrix", false);
-		hudShader.loadUniformMat4f(new Matrix4f(), "modelViewMatrix", false);
-
-		hud.getMesh().draw(GL_TRIANGLES);
+			Matrix4f orthographicProjectionMatrix = MatrixUtils.orthographicProjectionMatrix(0, -game.getWindowWidth(), -game.getWindowHeight(), 0.0f, -1.0f, 1.0f);
+			hudShader.loadUniformMat4f(orthographicProjectionMatrix, "projectionMatrix", false);
+			hudShader.loadUniformMat4f(new Matrix4f(), "modelViewMatrix", false);
+			hud.getMesh().draw(GL_TRIANGLES);
 		glUseProgram(0);
 	}
 
