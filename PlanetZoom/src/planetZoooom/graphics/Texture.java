7a4ebe595a6e169a2dfd6517cc0e9e0b73a52d39
Peter <gamblegearbox@googlemail.com>
2015-07-04 21:28:14 +0200

    activated bilinear texture filter, added new sun texture

--- PlanetZoom/src/planetZoooom/graphics/Texture.java
+++ PlanetZoom/src/planetZoooom/graphics/Texture.java
@@ -40,8 +40,8 @@ public class Texture
 			textureID = glGenTextures();
 			glBindTexture(GL_TEXTURE_2D, textureID);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 			glBindTexture(GL_TEXTURE_2D, 0);
 			
 			in.close();
