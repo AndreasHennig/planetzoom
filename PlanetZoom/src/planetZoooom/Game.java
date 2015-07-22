dedfbd78b09ff62374fb49cde1f1e98862d148f3
TheGreenTea <David.schmoecker@gmail.com>
2015-07-22 16:10:17 +0200

    Implemented new HUD functionalty

--- PlanetZoom/src/planetZoooom/Game.java
+++ PlanetZoom/src/planetZoooom/Game.java
@@ -1,7 +1,25 @@
 package planetZoooom;
 
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL11.GL_CCW;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_CW;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_LINES;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_CONSTANT_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_POINTS;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_VENDOR;
+import static org.lwjgl.opengl.GL11.GL_VERSION;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glFrontFace;
+import static org.lwjgl.opengl.GL11.glGetString;
+import static org.lwjgl.opengl.GL20.GL_SHADING_LANGUAGE_VERSION;
 import static org.lwjgl.opengl.GL20.glUseProgram;
 
 import org.lwjgl.glfw.GLFW;
@@ -10,6 +28,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import planetZoooom.engine.CoreEngine;
 import planetZoooom.engine.Renderer;
+import planetZoooom.gameContent.Atmosphere;
 import planetZoooom.gameContent.BillBoard;
 import planetZoooom.gameContent.FreeCamera;
 import planetZoooom.gameContent.HeadsUpDisplay;
@@ -173,26 +192,92 @@ public class Game implements IGame
 
 	private void updateHud(int mode)
 	{
-//		//TODO Distance to surface shall consider noise
+
+		switch(mode)
+		{
+			case HUD_MODE_OFF:
+			{
+				hud.update("");
+				return;
+			}
+		
+			case HUD_MODE_INFO:
+			{
+				hud.update(getInfoHUDText());
+				return;
+			}
+			
+			case HUD_MODE_NOISE:
+			{
+				hud.update(getNoiseHUDText());
+				return;
+			}
+			
+			case HUD_MODE_ATMOSPHERE:
+			{
+				hud.update(getAtmosphereHUDText());
+				return;
+			}
+		}
+		
+		throw new IllegalArgumentException();
+	}
+	
+	private String getInfoHUDText()
+	{
+		//TODO Distance to surface shall consider noise
 		int triangleCount = planet.getSphere().getTriangleCount();
 		int totalTriangleCount = planet.getSphere().getTotalTriangleCount();
 		double trianglePercentage = triangleCount * 100 / (double) totalTriangleCount;
 		
-		String text = String.format(
-							"Mode %d\n"
-							+ "Distance: %.2f\n"
-							+ "Triangles: %d / %d (%.2f%%)\n"
-							+ "Vertices: %d\n"
-							+ "FPS: %d\n"
-							+ "Subdivisions: %d",
-							mode,
-							GameUtils.getDistanceBetween(planet.getPosition(), Info.camera.getPosition()) - planet.getRadius(), 
-							triangleCount, totalTriangleCount, trianglePercentage,
-							planet.getSphere().getVertexCount(),
-							game.timer.getFPS(),
-							planet.getSphere().getSubdivisions());
-
-		hud.update(text);
+		return  String.format(
+				"General information\n\n"
+				+ "Distance: %.2f\n"
+				+ "Triangles: %d / %d (%.2f%%)\n"
+				+ "Vertices: %d\n"
+				+ "Subdivisions: %d\n"
+				+ "FPS: %d",
+				GameUtils.getDistanceBetween(planet.getPosition(), Info.camera.getPosition()) - planet.getRadius(), 
+				triangleCount, totalTriangleCount, trianglePercentage,
+				planet.getSphere().getVertexCount(),
+				planet.getSphere().getSubdivisions(),
+				game.timer.getFPS());
+	}
+	
+	private String getAtmosphereHUDText()
+	{
+		Atmosphere atmosphere = planet.getAtmosphere();
+		return  String.format(
+				"Atmosphere properties\n\n"
+				+ "Mie Scattering: %.3f\n"
+				+ "Rayleigh Scattering: %.3f\n"
+				+ "Wavelength red:   %.3f\n"
+				+ "Wavelength green: %.3f\n"
+				+ "Wavelength blue:  %.3f\n",
+				atmosphere.getMieScattering(),
+				atmosphere.getRayleighScattering(),
+				atmosphere.getWaveLengthRed(),
+				atmosphere.getWaveLengthGreen(),
+				atmosphere.getWaveLengthBlue());
+				
+	}
+	
+	private String getNoiseHUDText()
+	{
+		return String.format(
+				"Noise properties\n\n" +
+				"Mountain Height: %.4f\n" + 
+				"Seed: %.2f\n" +
+				"Wavelength: %.2f\n" + 
+				"Octaves: %d\n" + 
+				"Amplitude: %.2f\n" 
+				,
+				planet.getMountainHeight(),
+				planet.getNoiseSeed(),
+				planet.getLambdaBaseFactor(),
+				planet.getOctaves(), 
+				planet.getAmplitude()
+				);
 	}
 	
 	private void drawSun()
@@ -302,29 +387,67 @@ public class Game implements IGame
 		if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_9)){ updateSphere = !updateSphere; }
 		if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_TAB)){ hudMode = (hudMode + 1) % 4; }
 		
-		if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_O))
-			planet.setAmplitude(planet.getAmplitude() + 0.02f);
-		else if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_L))
-			planet.setAmplitude(planet.getAmplitude() - 0.02f);
-		
-		if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_I))
-			planet.setOctaves(planet.getOctaves() + 1);
-		else if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_K))
-			planet.setOctaves(planet.getOctaves() - 1);
-		
-		if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_U))
-			planet.setLambdaBaseFactor(planet.getLambdaBaseFactor() + 0.001f);
-		else if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_J))
-			planet.setLambdaBaseFactor(planet.getLambdaBaseFactor() - 0.001f);
-		
-		if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_Y))
-			planet.setNoiseSeed(planet.getNoiseSeed() + planet.getRadius() / 1000);
-		else if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_H))
-			planet.setNoiseSeed(planet.getNoiseSeed() - planet.getRadius() / 1000);
-		
-		if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_T))
-			planet.setMountainHeight(planet.getMountainHeight() + 0.0005f);
-		else if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_G))
-			planet.setMountainHeight(planet.getMountainHeight() - 0.0005f);
+		switch(hudMode)
+		{
+			case HUD_MODE_NOISE:
+			{
+				if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_O))
+					planet.setAmplitude(planet.getAmplitude() + 0.02f);
+				else if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_L))
+					planet.setAmplitude(planet.getAmplitude() - 0.02f);
+				
+				if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_I))
+					planet.setOctaves(planet.getOctaves() + 1);
+				else if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_K))
+					planet.setOctaves(planet.getOctaves() - 1);
+				
+				if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_U))
+					planet.setLambdaBaseFactor(planet.getLambdaBaseFactor() + 0.001f);
+				else if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_J))
+					planet.setLambdaBaseFactor(planet.getLambdaBaseFactor() - 0.001f);
+				
+				if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_Y))
+					planet.setNoiseSeed(planet.getNoiseSeed() + planet.getRadius() / 1000);
+				else if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_H))
+					planet.setNoiseSeed(planet.getNoiseSeed() - planet.getRadius() / 1000);
+				
+				if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_T))
+					planet.setMountainHeight(planet.getMountainHeight() + 0.0005f);
+				else if(Keyboard.isKeyPressed(GLFW.GLFW_KEY_G))
+					planet.setMountainHeight(planet.getMountainHeight() - 0.0005f);
+				break;
+			}
+			case HUD_MODE_ATMOSPHERE:
+			{
+				Atmosphere atmosphere = planet.getAtmosphere();
+				
+				if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_T))
+					atmosphere.setMieScattering(atmosphere.getMieScattering() + 0.01f);
+				else if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_G))
+					atmosphere.setMieScattering(atmosphere.getMieScattering() - 0.01f);
+				
+				if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_Y))
+					atmosphere.setRayleighScattering(atmosphere.getRayleighScattering() + 0.01f);
+				else if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_H))
+					atmosphere.setRayleighScattering(atmosphere.getRayleighScattering() - 0.01f);
+				
+				if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_U))
+					atmosphere.setWaveLengthRed(atmosphere.getWaveLengthRed() + 0.01f);
+				else if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_J))
+					atmosphere.setWaveLengthRed(atmosphere.getWaveLengthRed() - 0.01f);
+				
+				if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_I))
+					atmosphere.setWaveLengthGreen(atmosphere.getWaveLengthGreen() + 0.01f);
+				else if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_K))
+					atmosphere.setWaveLengthGreen(atmosphere.getWaveLengthGreen() - 0.01f);
+				
+				if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_O))
+					atmosphere.setWaveLengthBlue(atmosphere.getWaveLengthBlue() + 0.01f);
+				else if(Keyboard.isKeyPressedWithReset(GLFW.GLFW_KEY_L))
+					atmosphere.setWaveLengthBlue(atmosphere.getWaveLengthBlue() - 0.01f);
+				break;
+			}
+		}
+	
 	}
 }
--- PlanetZoom/src/planetZoooom/gameContent/Planet.java
+++ PlanetZoom/src/planetZoooom/gameContent/Planet.java
@@ -7,7 +7,7 @@ import planetZoooom.interfaces.IGameObjectListener;
 import planetZoooom.utils.CustomNoise;
 import planetZoooom.utils.Info;
 
-public class Planet implements IGameObjectListener 
+public class Planet implements IGameObjectListener
 {
 	private final static float MIN_AMPLITUDE = 1;
 	private final static float MIN_LAMBDA_BASE_FACTOR = 0.1f;
@@ -15,25 +15,25 @@ public class Planet implements IGameObjectListener
 	private final static float MIN_MOUNTAIN_HEIGHT = 0.0214f;
 	private final static int MIN_TRIANGLES = 10000;
 	final static float CAM_COLLISION_OFFSET = 30;
-	
+
 	private MasterSphere sphere;
 	private Vector3f position;
 	private Atmosphere atmosphere;
-	
+
 	private float amplitude;
 	private int octaves;
 	private float lambdaBaseFactor;
 	private float noiseSeed;
 	private float mountainHeight;
-	
-	public Planet(float radius, Vector3f position) 
+
+	public Planet(float radius, Vector3f position)
 	{
 		this.position = position;
 		this.sphere = new MasterSphere(radius, MIN_TRIANGLES);
 		this.atmosphere = new Atmosphere(this);
-		
+
 		sphere.addListener(this);
-		
+
 		lambdaBaseFactor = 0.75f;
 		octaves = 3;
 		amplitude = 1.77f;
@@ -44,177 +44,177 @@ public class Planet implements IGameObjectListener
 	public void update()
 	{
 		sphere.update();
-		
+
 		handleCollision();
 	}
-	
-	public float getAmplitude() {
+
+	public float getAmplitude()
+	{
 		return amplitude;
 	}
 
-	public void setAmplitude(float amplitude) {
-		if(amplitude < MIN_AMPLITUDE)
+	public void setAmplitude(float amplitude)
+	{
+		if (amplitude < MIN_AMPLITUDE)
 			this.amplitude = MIN_AMPLITUDE;
 		else
 			this.amplitude = amplitude;
-		
-		System.out.printf("Noise Amplitude: %.2f\n", this.amplitude);
 	}
 
-	public int getOctaves() {
+	public int getOctaves()
+	{
 		return octaves;
 	}
 
-	public void setOctaves(int octaves) {
-		if(octaves < MIN_OCTAVES)
+	public void setOctaves(int octaves)
+	{
+		if (octaves < MIN_OCTAVES)
 			this.octaves = MIN_OCTAVES;
 		else
 			this.octaves = octaves;
-		
-		System.out.println("Noise Octaves: " + this.octaves);
 	}
 
-	public float getLambdaBaseFactor() {
+	public float getLambdaBaseFactor()
+	{
 		return lambdaBaseFactor;
 	}
 
-	public void setLambdaBaseFactor(float lambdaBaseFactor) {
-		if(lambdaBaseFactor < MIN_LAMBDA_BASE_FACTOR)
+	public void setLambdaBaseFactor(float lambdaBaseFactor)
+	{
+		if (lambdaBaseFactor < MIN_LAMBDA_BASE_FACTOR)
 			this.lambdaBaseFactor = MIN_LAMBDA_BASE_FACTOR;
 		else
 			this.lambdaBaseFactor = lambdaBaseFactor;
-		
-		System.out.printf("Noise Lambda: %.2f\n",this.lambdaBaseFactor);
 	}
 
-	public float getNoiseSeed() {
+	public float getNoiseSeed()
+	{
 		return noiseSeed;
 	}
 
-	public void setNoiseSeed(float noiseSeed) {
+	public void setNoiseSeed(float noiseSeed)
+	{
 		this.noiseSeed = noiseSeed;
-		
-		System.out.println("Noise Seed: " + this.noiseSeed);
 	}
 
-	public void setAtmosphere(Atmosphere atmosphere) {
+	public void setAtmosphere(Atmosphere atmosphere)
+	{
 		this.atmosphere = atmosphere;
 	}
-	
-	public float getMountainHeight() {
+
+	public float getMountainHeight()
+	{
 		return mountainHeight;
 	}
 
-	public void setMountainHeight(float mountainHeight) {
-		if(mountainHeight < MIN_MOUNTAIN_HEIGHT)
+	public void setMountainHeight(float mountainHeight)
+	{
+		if (mountainHeight < MIN_MOUNTAIN_HEIGHT)
 			this.mountainHeight = MIN_MOUNTAIN_HEIGHT;
 		else
 			this.mountainHeight = mountainHeight;
-		
-		System.out.printf("Mountain Height: %.4f %%\n", this.mountainHeight);
 	}
-	
 
+	// public void update(int subdivisions)
+	// {
+	// sphere.update(subdivisions, Info.camera.getLookAt());
+	// }
+	//
+	// public void update(float planetCamDistance, boolean adjustCamSpeed)
+	// {
+	// float subdivisionCoefficient =
+	// GameUtils.getDistanceCoefficient(planetCamDistance);
+	//
+	// int subdivisions = (int) (subdivisionCoefficient / 1.2 *
+	// Sphere.MAX_SUBDIVISIONS);
+	//
+	// // clamp
+	// subdivisions = subdivisions < Sphere.MIN_SUBDIVISIONS ?
+	// Sphere.MIN_SUBDIVISIONS : subdivisions;
+	// this.update(subdivisions);
+	//
+	// // TODO: adjust cam speed with subdivisionCoefficient
+	// // if adjustCamSpeed is true
+	//
+	// handleCollision();
+	// }
 
-//	public void update(int subdivisions)
-//	{
-//		sphere.update(subdivisions, Info.camera.getLookAt());
-//	}
-//
-//	public void update(float planetCamDistance, boolean adjustCamSpeed) 
-//	{
-//		float subdivisionCoefficient = GameUtils.getDistanceCoefficient(planetCamDistance);
-//
-//		int subdivisions = (int) (subdivisionCoefficient / 1.2 * Sphere.MAX_SUBDIVISIONS);
-//
-//		// clamp
-//		subdivisions = subdivisions < Sphere.MIN_SUBDIVISIONS ? Sphere.MIN_SUBDIVISIONS : subdivisions;
-//		this.update(subdivisions);
-//
-//		// TODO: adjust cam speed with subdivisionCoefficient 
-//		// if adjustCamSpeed is true
-//		
-//		handleCollision();
-//	}
-	
-	private void handleCollision() {
+	private void handleCollision()
+	{
 		Vector3f cam = Info.camera.getPosition();
 		Vector3f planet = getPosition();
 		Vector3f planetToCam = new Vector3f();
-		
+
 		Vector3f.sub(cam, planet, planetToCam);
-		
+
 		float actualCamDistance = planetToCam.length();
 		planetToCam.normalise().scale(this.getRadius());
-		
-		double noise = CustomNoise.perlinNoise(planetToCam.x + noiseSeed, 
-											   planetToCam.y + noiseSeed, 
-											   planetToCam.z + noiseSeed, 
-											   octaves, 
-											   getLambda(getRadius()), 
-											   amplitude);
-		
-		if(noise < 0)
+
+		double noise = CustomNoise.perlinNoise(planetToCam.x + noiseSeed, planetToCam.y + noiseSeed, planetToCam.z + noiseSeed, octaves, getLambda(getRadius()), amplitude);
+
+		if (noise < 0)
 			noise = 0;
-		
+
 		noise *= mountainHeight * getRadius();
-		
+
 		float minCamDistance = (float) (getRadius() + noise);
-		
-		if(actualCamDistance < minCamDistance + CAM_COLLISION_OFFSET) {
+
+		if (actualCamDistance < minCamDistance + CAM_COLLISION_OFFSET)
+		{
 			planetToCam.normalise();
 			planetToCam.scale(-(actualCamDistance - minCamDistance) + CAM_COLLISION_OFFSET);
-			
+
 			Vector3f.add(cam, planetToCam, cam);
 		}
 	}
 
-	public float getRadius() 
+	public float getRadius()
 	{
 		return sphere.getRadius();
 	}
 
-	public Vector3f getPosition() 
+	public Vector3f getPosition()
 	{
 		return position;
 	}
-	
-	public int getTotalTriangleCount() 
+
+	public int getTotalTriangleCount()
 	{
 		return sphere.getTriangleCount();
 	}
-	
-	public int getVertexCount() 
+
+	public int getVertexCount()
 	{
 		return sphere.getVertexCount();
 	}
-	
+
 	public Atmosphere getAtmosphere()
 	{
 		return atmosphere;
 	}
-	
+
 	public MasterSphere getSphere()
 	{
 		return sphere;
 	}
 
-	private float getLambda(float planetRadius) {
+	private float getLambda(float planetRadius)
+	{
 		return lambdaBaseFactor * planetRadius;
 	}
-	
+
 	@Override
-	public void vertexCreated(Vector3f v) 
+	public void vertexCreated(Vector3f v)
 	{
 		float planetRadius = this.getRadius();
-		
+
 		final float lambda = lambdaBaseFactor * planetRadius;
 
 		float noise = (float) CustomNoise.perlinNoise(v.x + noiseSeed, v.y + noiseSeed, v.z + noiseSeed, octaves, lambda, amplitude);
 
-		if(noise < 0)
+		if (noise < 0)
 			noise = 0;
-		
+
 		// 0.14 % = 8 km von 6000 km
 		v.scale(1 + noise * mountainHeight);
 	}
