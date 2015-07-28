e2cbe7ddffa14264812a376fe57d011fdc998e08
TheGreenTea <David.schmoecker@gmail.com>
2015-07-28 03:23:54 +0200

    Atmosphere now takes the editable values into account and WIP on transforming to LUTs

--- PlanetZoom/src/planetZoooom/gameContent/Atmosphere.java
+++ PlanetZoom/src/planetZoooom/gameContent/Atmosphere.java
@@ -14,7 +14,7 @@ public class Atmosphere
 	private Vector3f position;
 	
 	private static final int ATMOSPHERE_SPHERE_SUBDIVISIONS = 6; 
-	private static final float ATMOSPHERE_PLANET_DISTANCE = 0.025f;
+	private static final float ATMOSPHERE_PLANET_DISTANCE = 0.255f;
 	
 	private static final int SAMPLE_RAYS = 2;						// Number of sample rays to use in integral equation
 	private static final float SUN_BRIGHTNESS = 10.0f;
@@ -22,26 +22,14 @@ public class Atmosphere
 	private static final float EXPOSURE = 2.0f;
 	private static final float RAYLEIGH_SCALE_DEPTH = 0.25f;
 	private static final float MIE_SCALE_DEPTH = 0.1f;
-		
-	private static final float WAVELENGTH_RED = 0.65f;
-	private static final float WAVELENGTH_GREEN = 0.57f;
-	private static final float WAVELENGTH_BLUE = 0.475f;
-	private static final float RAYLEIGH_SCATTERING = 0.0025f;		// Rayleigh scattering constant (Kr)
-	private static final float MIE_SCATTERING = 0.0010f;			// Mie scattering constant (Km)
-	
+			
 	private float rayleighScattering;
 	private float mieScattering;
 	private float waveLengthRed;
 	private float waveLengthGreen;
 	private float waveLengthBlue;
 	
-	private static final float[] WAVELENGTHS = new float[] 
-			{WAVELENGTH_RED, 
-			WAVELENGTH_GREEN, 
-			WAVELENGTH_BLUE, 
-			(float) Math.pow(WAVELENGTH_RED, 4), 
-			(float) Math.pow(WAVELENGTH_GREEN, 4), 
-			(float) Math.pow(WAVELENGTH_BLUE, 4)};
+	private float[] wavelengths = new float[3];
 	
 	
 	public Atmosphere(Planet planet)
@@ -49,15 +37,74 @@ public class Atmosphere
 		sphere = new StaticSphere(ATMOSPHERE_SPHERE_SUBDIVISIONS, planet.getRadius() * (1 + ATMOSPHERE_PLANET_DISTANCE));
 		position = planet.getPosition();
 		modelMatrix = new Matrix4f().translate(position);
+		
+		setWaveLengthRed(0.65f);
+		setWaveLengthGreen(0.57f);
+		setWaveLengthBlue(0.475f);
+		mieScattering = 0.001f;
+		rayleighScattering = 0.0025f;
 	}
+	
+	/**
+	 * if rayleigh is true rayleigh scattering is calculated 
+	 * otherwise mie scattering is calculated
+	 * returns the "optical depth" that is the average atmospheric density across the ray from point Pa to point Pb multiplied by the length of the ray
+	 */
+	private void outScattering(boolean rayleigh, Vector3f pa, Vector3f pb, int samples)
+	{
+
+		
+		/*
+		 * 
+		 * The scattering equations have nested integrals that are impossible to solve analytically; 
+		 * fortunately, it's easy to numerically compute the value of an integral with techniques such as the trapezoid rule. 
+		 * Approximating an integral in this manner boils down to a weighted sum calculated in a loop. Imagine a line segment on a graph: 
+		 * break up the segment into n sample segments and evaluate the integrand at the center point of each sample segment. 
+		 * Multiply each result by the length of the sample segment and add them all up. 
+		 * Taking more samples makes the result more accurate, but it also makes the integral more expensive to calculate.
+		 */
+		
+		Vector3f ray = new Vector3f();
+		Vector3f segment = new Vector3f();
+		Vector3f.sub(pb, pa, ray);
+		ray.scale(1.0f / samples);
+		float offset = (1.0f / samples) / 2.0f;
+		float segmentLength = segment.scale(1.0f / samples).length();
+		double sum = 0;
+		//calculate out-scattering integral
+		
+		for(int i = 0; i < samples; i++)
+		{
+			segment.x = ray.x;
+			segment.y = ray.y;
+			segment.z = ray.z;
+			segment.scale(offset + i * (1.0f / samples));
+		
+			//set height parameter so 0 equals sea level and 1 is the top of the atmosphere
+			sum += calculateAtmosphericDenisity(0, rayleigh) * segmentLength; 
+		}
+	}
+	
+	private void createLUT()
+	{
+		
+	}
+	private double calculateAtmosphericDenisity(float height, boolean rayleigh)
+	{
+		if(rayleigh)
+			return Math.exp(height / RAYLEIGH_SCALE_DEPTH);
+		else
+			return Math.exp(height / MIE_SCALE_DEPTH);
+	}
+
 	
 	public void loadSpecificUniforms(ShaderProgram atmosphereShader)
 	{
-		atmosphereShader.loadUniformVec3f(new Vector3f(1.0f/WAVELENGTHS[3], 1.0f/WAVELENGTHS[4], 1.0f/WAVELENGTHS[5]), "inverseWavelength");
-		atmosphereShader.loadUniform1f(MIE_SCATTERING * SUN_BRIGHTNESS, "mieScattering");
-		atmosphereShader.loadUniform1f((float)(MIE_SCATTERING * 4 * Math.PI), "mieScattering4Pi");
-		atmosphereShader.loadUniform1f(RAYLEIGH_SCATTERING * SUN_BRIGHTNESS, "rayleighScattering");
-		atmosphereShader.loadUniform1f((float)(RAYLEIGH_SCATTERING * 4 * Math.PI), "rayleighScattering4Pi");
+		atmosphereShader.loadUniformVec3f(new Vector3f(wavelengths[0],wavelengths[1], wavelengths[2]), "inverseWavelength");
+		atmosphereShader.loadUniform1f(mieScattering * SUN_BRIGHTNESS, "mieScattering");
+		atmosphereShader.loadUniform1f((float)(mieScattering * 4 * Math.PI), "mieScattering4Pi");
+		atmosphereShader.loadUniform1f(rayleighScattering * SUN_BRIGHTNESS, "rayleighScattering");
+		atmosphereShader.loadUniform1f((float)(rayleighScattering * 4 * Math.PI), "rayleighScattering4Pi");
 		atmosphereShader.loadUniform1f(sphere.getRadius(), "atmosphereRadius");
 		atmosphereShader.loadUniform1f(RAYLEIGH_SCALE_DEPTH, "scaleDepth");
 		atmosphereShader.loadUniform1f(MIE_PHASE_ASYMETRY_FACTOR, "miePhaseAsymetryFactor");
@@ -107,6 +154,7 @@ public class Atmosphere
 	public void setWaveLengthRed(float waveLengthRed)
 	{
 		this.waveLengthRed = waveLengthRed;
+		wavelengths[0] = (float) (1.0f / Math.pow(waveLengthRed, 4));
 	}
 
 	public float getWaveLengthGreen()
@@ -117,6 +165,7 @@ public class Atmosphere
 	public void setWaveLengthGreen(float waveLengthGreen)
 	{
 		this.waveLengthGreen = waveLengthGreen;
+		wavelengths[1] = (float) (1.0f / Math.pow(waveLengthGreen, 4));
 	}
 
 	public float getWaveLengthBlue()
@@ -127,8 +176,7 @@ public class Atmosphere
 	public void setWaveLengthBlue(float waveLengthBlue)
 	{
 		this.waveLengthBlue = waveLengthBlue;
-	}
-	
-	
+		wavelengths[2] = (float) (1.0f / Math.pow(waveLengthBlue, 4));
+	}	
 
 }
